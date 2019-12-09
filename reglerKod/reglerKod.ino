60826cec71a0f5e95275f3dfdbb08749584f16a6
abblinkas <43879956+abblinkas@users.noreply.github.com>
2019-12-09 12:47:02 +0100

    update reglerkod

--- reglerKod/reglerKod.ino
+++ reglerKod/reglerKod.ino
@@ -3,8 +3,9 @@
 #define dir 0
 #define hallGivarePin 13
 
-float rev, currentMillis, startMillis, diffMillis, currentPulse, startPulse, diffPulse, error, timeError, isValue, enginePower;
-float kP = 0.5, kI = 7500, setValue = 30, circumference = 3.7 * PI;
+float rev, currentMillis, startMillis, diffMillis, currentPulse, startPulse, diffPulse, error, timeError, isValue, currentTime, diffTime, previousTime;
+float kP = 0.001, kI = 10000, circumference = 3.7 * PI, setValue = 0, enginePower = 0;
+bool drive = false;
 
 void onConnectionEstablished();
 
@@ -31,9 +32,13 @@ void setup() {
 
 void onConnectionEstablished()
 {
-  client.subscribe("linus.kasper@abbindustrigymnasium.se/logger", [] (const String &payload)
-  {
-    //enginePower = payload.toInt();
+  client.subscribe("linus.kasper@abbindustrigymnasium.se/logger", [] (const String &payload){
+    if (payload.endsWith("/")){
+      
+    }else{
+      setValue = payload.toInt();
+      drive = true;
+    }
   });
 }
 
@@ -63,9 +68,14 @@ void loop() {
   client.loop();
   digitalWrite(dir, HIGH);
   analogWrite(pwm, enginePower);
-  isValue = getVelocity();
-  error = setValue - isValue;
-  timeError += error * dTime();
-  enginePower = (kP * error) + (kI * timeError);
-  client.publish("linus.kasper@abbindustrigymnasium.se/logger","linus isValue " + String(isValue));
+  if (drive == true){
+    isValue = getVelocity();
+    error = setValue - isValue;
+    timeError += error * dTime();
+    enginePower = (kP * error) + (kI * timeError);
+    client.publish("linus.kasper@abbindustrigymnasium.se/logger","linus " + String(isValue) + " " +String(enginePower) + " " + String(error) + " /");
+    if (enginePower > 1023){
+      enginePower = 1023;
+    }
+  }
 }
